69b70bac13ece2e236871f260ab73c4ceb569258
Chris Mipi <christopher.mipi@gmail.com>
2021-03-31 16:42:13 +0200

    update a record

--- fn-update/index.js
+++ fn-update/index.js
@@ -1,13 +1,37 @@
+require('dotenv').config()
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    const endpoint = process.env.ACCOUNT_URI;
+    const key = process.env.ACCOUNT_KEY;
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const client = new CosmosClient({ endpoint, key });
+    const { database } = await client.databases.createIfNotExists({ id: 'ToDoDB' });
+    const { container } = await database.containers.createIfNotExists({ id: 'Actions' });
+
+    const title = (req.query.title || (req.body && req.body.title));
+    const complete = (req.query.complete || (req.body && req.body.complete));
+    const id = context.bindingData.id;
+
+    const querySpec = {
+    query: 'SELECT * FROM Actions f WHERE  f.id = @id',
+    parameters: [
+            {
+                name: '@id',
+                value: id
+            }
+        ]
+    };
+
+    const { resources: results } = await container.items.query(querySpec).fetchAll();
+    const item = results[0];
+    item.title = title;
+    item.complete = complete;
+
+    await container.items.upsert(item);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: 'Saved'
     };
 }
