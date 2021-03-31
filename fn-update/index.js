e51d22e6992473c39ecd0cfd75584daeb747aab3
Chris Mipi <christopher.mipi@gmail.com>
2021-03-31 16:46:47 +0200

    errors

--- fn-update/index.js
+++ fn-update/index.js
@@ -2,36 +2,43 @@ require('dotenv').config()
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    const endpoint = process.env.ACCOUNT_URI;
-    const key = process.env.ACCOUNT_KEY;
+    try {
+        const endpoint = process.env.ACCOUNT_URI;
+        const key = process.env.ACCOUNT_KEY;
 
-    const client = new CosmosClient({ endpoint, key });
-    const { database } = await client.databases.createIfNotExists({ id: 'ToDoDB' });
-    const { container } = await database.containers.createIfNotExists({ id: 'Actions' });
+        const client = new CosmosClient({ endpoint, key });
+        const { database } = await client.databases.createIfNotExists({ id: 'ToDoDB' });
+        const { container } = await database.containers.createIfNotExists({ id: 'Actions' });
 
-    const title = (req.query.title || (req.body && req.body.title));
-    const complete = (req.query.complete || (req.body && req.body.complete));
-    const id = context.bindingData.id;
+        const title = (req.query.title || (req.body && req.body.title));
+        const complete = (req.query.complete || (req.body && req.body.complete));
+        const id = context.bindingData.id;
 
-    const querySpec = {
-    query: 'SELECT * FROM Actions f WHERE  f.id = @id',
-    parameters: [
-            {
-                name: '@id',
-                value: id
-            }
-        ]
-    };
+        const querySpec = {
+        query: 'SELECT * FROM Actions f WHERE  f.id = @id',
+        parameters: [
+                {
+                    name: '@id',
+                    value: id
+                }
+            ]
+        };
 
-    const { resources: results } = await container.items.query(querySpec).fetchAll();
-    const item = results[0];
-    item.title = title;
-    item.complete = complete;
+        const { resources: results } = await container.items.query(querySpec).fetchAll();
+        const item = results[0];
+        item.title = title;
+        item.complete = complete;
 
-    await container.items.upsert(item);
+        await container.items.upsert(item);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: 'Saved'
-    };
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: 'Saved'
+        };
+    } catch(error) {
+        context.res = {
+            status: 500, /* Defaults to 200 */
+            body: error
+        };
+    }
 }
